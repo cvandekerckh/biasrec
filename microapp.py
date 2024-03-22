3c131cd9ebdfb436189469d37369c6d814b71530
Corentin Vande Kerckhove <corentin.vandekerckhove@uclouvain.be>
2024-03-22 12:04:39 +0100

    create configuration handler actionable through .env file

--- microapp.py
+++ microapp.py
@@ -1,7 +1,13 @@
+import os
 from app import create_app,db
 from app.models import User, Product
+from config import configs
+from dotenv import load_dotenv
 
-app = create_app()
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+experiment_config = os.environ.get('EXPERIMENT_CONFIG') or 'default'
+app = create_app(config_class=configs[experiment_config])
 
 
 @app.shell_context_processor
