285d14e5409d0064b62cc98c92e476f4f561454d
Corentin Vande Kerckhove <corentin.vandekerckhove@uclouvain.be>
2024-04-08 15:11:32 +0200

    tried an attempt to delete tables in the child then parent process

--- scripts/reload_experiment.py
+++ scripts/reload_experiment.py
@@ -47,6 +47,41 @@ def populate_users():
 def populate_products():
     populate_db(Product, Cf.DATA_PATH_RAW / PRODUCT_FILENAME)
 
+def drop_tables_in_order(table_names, foreign_keys, engine):
+    # Function to perform topological sort
+    def topological_sort(graph):
+        visited = set()
+        stack = []
+
+        def dfs(node):
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+            stack.append(node)
+
+        for node in graph.keys():
+            if node not in visited:
+                dfs(node)
+        return stack
+
+    # Create a graph of foreign key dependencies
+    dependency_graph = {table: [] for table in table_names}
+    for table, fks in foreign_keys.items():
+        for fk in fks:
+            dependency_graph[fk['referred_table']].append(table)
+
+    # Perform topological sort to get the correct order of dropping tables
+    sorted_tables = topological_sort(dependency_graph)
+    print(sorted_tables)
+
+    # Drop tables in the sorted order
+    for table_name in sorted_tables:
+        if inspect(db.engine).has_table(table_name):
+            db.metadata.tables[table_name].drop(db.engine)
+        #engine.execute(f"DROP TABLE IF EXISTS {table_name};")
+
+
 def drop_all_tables():
     """Drop tables in correct order"""
     # Get metadata
@@ -55,38 +90,52 @@ def drop_all_tables():
     # Get all table names
     table_names = metadata.tables.keys()
     print(table_names)
+
     # Create a dictionary to store foreign key relationships
     foreign_keys = {}
-
-    # Inspect foreign key constraints
     inspector = inspect(db.engine)
     for table_name in table_names:
         try:
             foreign_keys[table_name] = inspector.get_foreign_keys(table_name)
         except NoSuchTableError:
-            continue
-            
-    for key in foreign_keys:
-        print(key)
-        print([fk['referred_table'] for fk in foreign_keys[key]])
-        print('\n')
+            foreign_keys[table_name] = []
 
-    # Drop tables in the correct order (from child to parent)
-    dropped_tables = set()
-    while len(dropped_tables) < len(table_names):
-        for table_name in table_names:
-            print(table_name)
-            if table_name not in dropped_tables:
-                if table_name in foreign_keys:
-                    print('if')
-                    dependent_tables = [fk['referred_table'] for fk in foreign_keys[table_name]]
-                    print(dependent_tables)
-                    if all(dep_table in dropped_tables for dep_table in dependent_tables):
-                        metadata.tables[table_name].drop(db.engine)
-                        dropped_tables.add(table_name)
-                else:
-                    print('else')
-                    dropped_tables.add(table_name)
+    drop_tables_in_order(table_names, foreign_keys, db.engine)
+
+    # print(table_names)
+    # # Create a dictionary to store foreign key relationships
+    # foreign_keys = {}
+
+    # # Inspect foreign key constraints
+    # inspector = inspect(db.engine)
+    # for table_name in table_names:
+    #     try:
+    #         foreign_keys[table_name] = inspector.get_foreign_keys(table_name)
+    #     except NoSuchTableError:
+    #         continue
+
+    # for key in foreign_keys:
+    #     print(key)
+    #     print([fk['referred_table'] for fk in foreign_keys[key]])
+    #     print('\n')
+
+    # # Drop tables in the correct order (from child to parent)
+    # dropped_tables = set()
+    # while len(dropped_tables) < len(table_names):
+    #     for table_name in table_names:
+    #         print(table_name)
+    #         if table_name not in dropped_tables:
+    #             if table_name in foreign_keys:
+    #                 print('if')
+    #                 dependent_tables = [fk['referred_table'] for fk in foreign_keys[table_name]]
+    #                 print(dependent_tables)
+    #                 if all(dep_table in dropped_tables for dep_table in dependent_tables):
+    #                     print('drop')
+    #                     metadata.tables[table_name].drop(db.engine)
+    #                     dropped_tables.add(table_name)
+    #             else:
+    #                 print('else')
+    #                 dropped_tables.add(table_name)
 
 def reload_databases():
     with app.app_context():
