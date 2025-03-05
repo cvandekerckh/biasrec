49221cbe3f079111708a106eb955044a5dcf1cf4
CamilleCharles <camille.charles@uclouvain.be>
2025-03-05 17:41:12 +0100

    Modification du style des boutons et de la page product_category

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 9
     DATA_PATH = Path('data/test')
     DATA_PATH_RAW = DATA_PATH / 'raw'
     DATA_PATH_OUT = DATA_PATH / 'out'
